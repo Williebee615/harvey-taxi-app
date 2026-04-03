bd09d2dddbfde6af36ba74e49c86f58537ef621b
Williebee615 <willieharvey813@gmail.com>
2026-04-03 18:20:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const COMMANDS = './commands.json'const express = require('express')
 const cors = require('cors')
 const fs = require('fs')
 const path = require('path')
